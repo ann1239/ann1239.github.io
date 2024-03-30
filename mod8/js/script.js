65ba4bcb2224a799490f7fb926a332555ab1ed64
ann1239 <124619552+ann1239@users.noreply.github.com>
2024-03-30 16:26:44 +0530

    Add files via upload

---
+++ mod8/js/script.js
@@ -0,0 +1,9 @@
+function calculateAverage() {
+  var num1 = parseFloat(document.getElementById('num1').value);
+  var num2 = parseFloat(document.getElementById('num2').value);
+  var num3 = parseFloat(document.getElementById('num3').value);
+  
+  var average = (num1 + num2 + num3) / 3;
+  
+  document.getElementById('average').innerText = average.toFixed(2);
+}
